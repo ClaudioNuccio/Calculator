85e8c7ce1455ed7ea65a4db3f1ad9445ade62869
Alessandro <sandroscro@libero.it>
2023-10-18 10:24:42 +0200

    Creazione funzione pari e dispari

---
+++ pariDispari.java
@@ -0,0 +1,9 @@
+public class pariDispari {
+    public static void pariDispari(int x){
+        if(x%2==0){
+            System.out.println("Il numero "+x+" è pari");
+        } else {
+            System.out.println("Il numero "+x+" è dispari");
+        }
+    }
+}
