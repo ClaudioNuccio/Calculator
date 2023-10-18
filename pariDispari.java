3263146ef56eafee862727e7a12b30d58ea36ff7
Alessandro <sandroscro@libero.it>
2023-10-18 10:55:50 +0200

    Correzione pariDispari

--- pariDispari.java
+++ pariDispari.java
@@ -1,9 +1,9 @@
 public class pariDispari {
     public static void pariDispari(int x){
         if(x%2==0){
-            System.out.print("Il numero "+x+typeof(x)" è pari");
+            System.out.print("Il numero "+x+typeof(x)+" è pari");
         } else {
-            System.out.print("Il numero "+x+typeof(x)" è dispari");
+            System.out.print("Il numero "+x+typeof(x)+" è dispari");
         }
 
     }
